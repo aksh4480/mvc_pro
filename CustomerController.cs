6e8a2bfcb23328ffcc2a5a097d187db87ba7b0c6
aksh4480 <86340798+aksh4480@users.noreply.github.com>
2021-08-16 15:05:26 +0530

    firat_mvc_pro

---
+++ CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace first__MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = new List<string>()
+            {
+                "Rishabh",
+                "Arjun",
+                "Himan",
+                "Krish"
+
+            };
+            ViewBag.customers = customers;
+            return View();
+        }
+    }
+}
